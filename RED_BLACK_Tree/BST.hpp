0d5ac42f2aa9a46852e316d3675ea226d0744b0c
Fathiya Jamia <fathiyajamaa051@gmail.com>
2022-11-02 18:04:59 +0100

    feat: add insertion and deletion functions (optimized version)

--- RED_BLACK_Tree/BST.hpp
+++ RED_BLACK_Tree/BST.hpp
@@ -10,7 +10,13 @@ namespace ft
         BLACK
     };
 
-    template<typename T, class Compare = std::less<T>, class Allocator = std::allocator<T> >
+    enum _child{
+        LEFT,
+        RIGHT,
+        NONE
+    };
+
+    template<typename T, class Compare = std::less<T> >
     class BST
     {
 
@@ -19,21 +25,20 @@ namespace ft
         typedef Compare     cmp;
         typedef size_t      size_type;
 
-        struct node
+        struct _node
         {
             value_type _data;
-            node* _parent;
-            node* _left_c;
-            node* _right_c;
+            _node* _parent;
+            _node* _left_c;
+            _node* _right_c;
             color _color;
         };
-        typedef std::allocator<node>   alloc_type;
+        typedef std::allocator<_node>   alloc_type;
 
-        typedef node* node_ptr;
-        typedef std::allocator<node> node_alloc;
+        typedef _node* node_ptr;
 
     public:
-        node_alloc  _alloc;
+        alloc_type  _alloc;
         node_ptr    _tree;
         node_ptr    _tnull;
         size_type   _size;
@@ -42,9 +47,8 @@ namespace ft
     public:
 
         BST() : _size(0) {
-            std::allocator<int> alloc;
             this->_tnull = _alloc.allocate(1);
-            _alloc.construct(this->_tnull, node());
+            _alloc.construct(this->_tnull, _node());
             this->_tnull->_right_c = nullptr;
             this->_tnull->_left_c = nullptr;
             this->_tnull->_parent = nullptr;
@@ -57,7 +61,7 @@ namespace ft
         node_ptr new_node(node_ptr node, value_type data = value_type())
         {
             node = _alloc.allocate(1);
-//            _alloc.construct(node, data);
+            _alloc.construct(node, _node());
             node->_data = data;
             node->_parent = _tnull;
             node->_left_c = _tnull;
@@ -72,13 +76,62 @@ namespace ft
                 node = new_node(node, data);
             }
             else if(data > node->_data) {
-                insert(data, node->_left_c);
-                node->_left_c->_parent = node;
-            }
-            else if(data < node->_data){
                 insert(data, node->_right_c);
                 node->_right_c->_parent = node;
             }
+            else if(data < node->_data){
+                insert(data, node->_left_c);
+                node->_left_c->_parent = node;
+            }
+        }
+
+        void delete_elem(value_type data, node_ptr& root){
+            if (root == _tnull || root == nullptr)
+                return;
+            if(data < root->_data)
+                delete_elem(data, root->_left_c);
+            else if (data > root->_data)
+                delete_elem(data, root->_right_c);
+            else{
+                if (is_leaf(root)){
+                    (child_position(root->_parent, root) == LEFT ? root->_parent->_left_c = _tnull : root->_parent->_right_c = _tnull);
+                    _alloc.destroy(root);
+                } else if (root->_right_c != _tnull && root->_left_c != _tnull){
+                    node_ptr tmp = successor(root->_left_c);
+                    root->_data = tmp->_data;
+                    if (!is_leaf(tmp))
+                    {
+                        tmp->_parent->_right_c = tmp->_left_c;
+                        tmp->_left_c->_parent = tmp->_parent;
+                    }
+                    else
+                        tmp->_parent->_right_c = _tnull;
+                    _alloc.destroy(tmp);
+                    _alloc.deallocate(tmp, 1);
+                } else{
+                    node_ptr tmp = root;
+                    node_ptr root_child = (root->_left_c ? root->_left_c : root->_right_c);
+                    root_child->_parent = root->_parent;
+                    root = root_child;
+                    _alloc.destroy(tmp);
+                    _alloc.deallocate(tmp, 1);
+                }
+            }
+        }
+
+        _child child_position(node_ptr parent, node_ptr child)
+        {
+            if (!parent || !child || parent == _tnull || child == _tnull)
+                return NONE;
+            if (parent->_left_c == child)
+                return LEFT;
+            else if (parent->_right_c == child)
+                return RIGHT;
+            return NONE;
+        }
+
+        bool is_leaf(node_ptr node){
+            return (node->_right_c == _tnull) && (node->_left_c == _tnull);
         }
 
         node_ptr search(value_type data, node_ptr root){
@@ -90,20 +143,49 @@ namespace ft
                 else if(data < root->_data)
                     return search(data, root->_left_c);
             }
-            return this->root;
+            return root;
         }
 
-        void preorder_traverse(node_ptr root){
-            if(root != _tnull){
-                preorder_traverse(root->_left_c);
+
+        void inorder_traverse(node_ptr root){
+            if(root != nullptr && root != _tnull){
+                inorder_traverse(root->_left_c);
                 std::cout << "data: "<< root->_data << std::endl;
                 std::cout << "parent data " << root->_parent->_data << std::endl;
                 std::cout << "color " << (root->_color == RED ? "RED": "BLACK") << std::endl;
                 std::cout << "\n";
-                preorder_traverse(root->_right_c);
+                inorder_traverse(root->_right_c);
             }
         }
 
+        void clear(node_ptr& root){
+            if (root == _tnull)
+                return;
+            clear(root->_right_c);
+            clear(root->_left_c);
+            _alloc.destroy(root);
+            _alloc.deallocate(root, 1);
+            root  = nullptr;
+        }
+
+        void clear_tree(node_ptr& root){
+            clear(root);
+            _alloc.destroy(_tnull);
+            _alloc.deallocate(_tnull, 1);
+            _tnull = nullptr;
+        }
+
+        node_ptr successor(node_ptr node){
+            while (node && node != _tnull && node->_right_c != _tnull)
+                node = node->_right_c;
+            return  node;
+        }
+
+        node_ptr predeccessor(node_ptr node){
+            while (node && node != _tnull && node->_left_c != _tnull)
+                node = node->_left_c;
+            return  node;
+        }
 
     };
 };
