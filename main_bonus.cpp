8332f8e6acdb3a0561d8c3f60665167e4d701189
Fathiya Jamia <fathiyajamaa051@gmail.com>
2022-11-22 14:13:33 +0100

    add main_bounus

--- main_bonus.cpp
+++ main_bonus.cpp
@@ -1,3 +1,20 @@
 //
 // Created by Fathiyat olatokunbo Jamia on 11/22/22.
 //
+#include <iostream>
+# include "tester/tester.hpp"
+
+bool printBoolResult(std::fstream& fs, bool stl_bool, bool ft_bool)
+{
+    fs << "\nBool result : \n";
+    fs << " - STL : " << (stl_bool ? "true" : "false") << std::endl;
+    fs << " - FT  : " << (ft_bool ? "true" : "false") << std::endl;
+
+    return (stl_bool == ft_bool);
+}
+
+
+int main()
+{
+    test_set();
+}
